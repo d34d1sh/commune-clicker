fb83a0d3dbbd37e08842087cdc69708c76138e1e
d34d1sh <80916465+d34d1sh@users.noreply.github.com>
2024-12-14 20:59:20 -0800

    Update commune.js

--- commune.js
+++ commune.js
@@ -550,7 +550,7 @@ if (isSmallScreenPortrait()) {
                 align: 'left'
             }
         );
-        asciiTitle.setScale(.9, 1);
+        asciiTitle.setScale(.8, 1);
         uiContainer.add(asciiTitle);
     }
 
